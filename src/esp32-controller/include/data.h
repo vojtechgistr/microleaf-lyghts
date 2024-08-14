600791b653610014d22438a1f9b6f9e0a6d3735e
Vojtěch Gistr (AdalberToo) <vojtech.gistr@gmail.com>
2024-08-14 14:40:56 +0200

    Update data.h

--- src/esp32-controller/include/data.h
+++ src/esp32-controller/include/data.h
@@ -3,11 +3,8 @@
 
 // This file contains the data that is used by the program
 
-#define NETWORK_SSID "Hospoda";      // your network SSID (name)
-#define NETWORK_PASSWORD "baqc0289"; // your network password
-
-// #define NETWORK_SSID "Gistr1";      // your network SSID (name)
-// #define NETWORK_PASSWORD "0949avexlogis"; // your network password
+#define NETWORK_SSID "MyName";      // your network SSID (name)
+#define NETWORK_PASSWORD "password"; // your network password
 
 #define LED_PIN 4
 #define NUM_LEDS 54
